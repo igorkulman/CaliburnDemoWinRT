6dec82f96aae3fa7eea22abc4097d9ebc6bb388c
Igor Kulman <igor@kulman.sk>
2013-07-26 15:48:49 +0200

    + first ViewModel done

---
+++ CaliburnDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Caliburn.Micro.Unity.WinRT.ViewModels;
+using PropertyChanged;
+
+namespace CaliburnDemo.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class MainViewModel: ViewModelBase
+    {
+        public string Title { get; set; }
+
+        public MainViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Caliburn Demo";
+        }
+    }
+}
