156b02bfe131b5cb4e1082fada6d05fb0e28fc21
Igor Kulman <igor@kulman.sk>
2013-08-07 16:05:33 +0200

    + saving and restoring state

--- CaliburnDemo/ViewModels/SecondPageViewModel.cs
+++ CaliburnDemo/ViewModels/SecondPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using Caliburn.Micro.Unity.WinRT.Code;
 using Caliburn.Micro.Unity.WinRT.ViewModels;
 using PropertyChanged;
 using Windows.UI.Popups;
@@ -11,9 +12,11 @@ using Windows.UI.Popups;
 namespace CaliburnDemo.ViewModels
 {
     [ImplementPropertyChanged]
-    public class SecondPageViewModel: ViewModelBase
+    public class SecondPageViewModel: ViewModelBase, IHaveState
     {
         public string Title { get; set; }
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
 
         public SecondPageViewModel(INavigationService navigationService) : base(navigationService)
         {
@@ -23,6 +26,18 @@ namespace CaliburnDemo.ViewModels
         public void GoBack()
         {
             this.navigationService.GoBack();
+        }
+
+        public override void LoadState(string Parameter, Dictionary<string, object> statePageState)
+        {
+            Property1 = (string)statePageState["Property1"];
+            Property2 = (string)statePageState["Property2"];
+        }
+
+        public override void SaveState(Dictionary<string, object> statePageState, List<Type> knownTypes)
+        {
+            statePageState.Add("Property1", Property1);
+            statePageState.Add("Property2", Property2);
         }
        
     }
