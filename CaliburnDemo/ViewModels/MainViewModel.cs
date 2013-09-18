db7b9ec64c3558c02592a6b42bdc5a6549f71743
Igor Kulman <igor@kulman.sk>
2013-09-18 12:52:10 +0200

    + products in view model

--- CaliburnDemo/ViewModels/MainViewModel.cs
+++ CaliburnDemo/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Caliburn.Micro;
 using Caliburn.Micro.Unity.WinRT.ViewModels;
+using CaliburnDemo.Data;
+using CaliburnDemo.Servcies;
 using PropertyChanged;
 using Windows.UI.Popups;
 
@@ -9,10 +11,14 @@ namespace CaliburnDemo.ViewModels
     public class MainViewModel: ViewModelBase
     {
         public string Title { get; set; }
+        public IObservableCollection<Product> Products { get; private set; }
+        private readonly IProductService _productService;
 
-        public MainViewModel(INavigationService navigationService) : base(navigationService)
+        public MainViewModel(INavigationService navigationService, IProductService productService) : base(navigationService)
         {
+            _productService = productService;
             Title = "Caliburn Demo";
+            Products = new BindableCollection<Product>(_productService.GetAll());
         }
 
         public void GoToSecondPage()
