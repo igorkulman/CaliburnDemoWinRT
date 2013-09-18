93b224c0860a308caa2c693ab87446db298411b7
Igor Kulman <igor@kulman.sk>
2013-09-18 12:51:41 +0200

    + product data class, interface and service implementation

---
+++ CaliburnDemo/Data/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using PropertyChanged;
+
+namespace CaliburnDemo.Data
+{
+    [ImplementPropertyChanged]
+    public class Product : PropertyChangedBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CaliburnDemo/Servcies/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CaliburnDemo.Data;
+
+namespace CaliburnDemo.Servcies
+{
+    public class ProductService: IProductService
+    {
+        private readonly List<Product> _products = new List<Product>()
+            {
+                new Product
+                    {
+                        Id = 1,
+                        Name = "Product 1"
+                    },
+                     new Product
+                    {
+                        Id = 2,
+                        Name = "Product 2"
+                    }
+            };
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public Product Get(int id)
+        {
+            return _products.SingleOrDefault(l => l.Id == id);
+        }
+    }
+}
